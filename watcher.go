7c1c819379ef9e211126433ed5635cffef48fb1e
Antanidoss <antosci2002@gmail.com>
2023-03-30 10:43:48 +0300

    Created watcher constructor

--- watcher.go
+++ watcher.go
@@ -1,11 +1,11 @@
 package fileWatcher
 
 type Watcher struct {
-	directoryPath          string
-	watchNestedDirectories bool
-	onCreatedFile          func(message EventFileWatcherMessage)
-	onRemoveFile           func(message EventFileWatcherMessage)
-	onAnyChange            func(message EventFileWatcherMessage)
+	DirectoryPath          string
+	WatchNestedDirectories bool
+	OnCreatedFile          func(message EventFileWatcherMessage)
+	OnRemoveFile           func(message EventFileWatcherMessage)
+	OnAnyChange            func(message EventFileWatcherMessage)
 }
 
 type NotificationType int64
@@ -16,7 +16,28 @@ const (
 )
 
 type EventFileWatcherMessage struct {
-	directoryPath    string
-	filePath         string
-	notificationType NotificationType
+	DirectoryPath    string
+	FilePath         string
+	NotificationType NotificationType
+}
+
+func (watcher Watcher) Start() {
+
+}
+
+func (watcher Watcher) Stop() {
+
+}
+
+func NewWatcher(directoryPath string, watchNestedDirectories bool, onCreatedFile func(message EventFileWatcherMessage), onRemoveFile func(message EventFileWatcherMessage), onAnyChange func(message EventFileWatcherMessage)) (*Watcher, error) {
+	if onCreatedFile == nil && onRemoveFile == nil && onAnyChange == nil {
+		panic("All event handlers are equal nil")
+	}
+
+	return &Watcher{
+		DirectoryPath:          directoryPath,
+		WatchNestedDirectories: watchNestedDirectories,
+		OnCreatedFile:          onCreatedFile,
+		OnRemoveFile:           onRemoveFile,
+		OnAnyChange:            onAnyChange}, nil
 }
